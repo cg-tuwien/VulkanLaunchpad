1b7bebe07e4a31a2517a899b123b7afcfb25a152
elias <61783913+elias1518693@users.noreply.github.com>
2023-01-21 12:17:34 +0100

    added update function without required paramters

--- Camera.cpp
+++ Camera.cpp
@@ -3,20 +3,18 @@
  */
 #include "Camera.h"
 #include <glm/gtx/quaternion.hpp>
-#include "VulkanLaunchpad.h"
 
-Camera::Camera(glm::mat4 projection_matrix)
-	: _viewMatrix(glm::mat4(1)), _projMatrix(projection_matrix), _mouseX(0), _mouseY(0), ooo(0.0f), ggg(0.0f), _strafe(glm::vec3(0)) 
+Camera::Camera(GLFWwindow* window, glm::mat4 projection_matrix)
+	: _window(window), _viewMatrix(glm::mat4(1)), _projMatrix(projection_matrix), _mouseX(0), _mouseY(0), ooo(0.0f), ggg(0.0f), _strafe(glm::vec3(0))
 { }
 
-Camera::Camera(int window_width, int window_height)
-	: Camera(vklCreatePerspectiveProjectionMatrix(glm::radians(60.0f), static_cast<float>(window_width) / static_cast<float>(window_height), 0.1f, 1000.0f))
+Camera::Camera(GLFWwindow* window, int window_width, int window_height)
+	: Camera(window, vklCreatePerspectiveProjectionMatrix(glm::radians(60.0f), static_cast<float>(window_width) / static_cast<float>(window_height), 0.1f, 1000.0f))
 { }
 
-Camera::Camera()
-	: Camera(1, 1)
+Camera::Camera(GLFWwindow* window)
+	: Camera(window,1, 1)
 { }
-
 Camera::~Camera()
 {
 }
@@ -103,6 +101,95 @@ void Camera::update(double x, double y, float zoom, bool dragging, bool strafing
 	glm::mat4 R1 = { 1 - 2 * O * O ,2 * o * O + 2 * oO * Oo,2 * o * Oo - 2 * oO * O,0,2 * o * O - 2 * oO * Oo,1 - 2 * o * o - 2 * Oo * Oo,2 * O * Oo + 2 * oO * o,0,2 * o * Oo + 2 * oO * O,2 * O * Oo - 2 * oO * o,1 - 2 * o * o - 2 * O * O,0,0,0,0,1
 	};
 	glm::mat4 R2 = { 1 -  O * O - 2 * Oo * Oo,2 * o * O + 2 * oO * Oo,2 * o * Oo - 2 * oO ,0,2 * o * O - 2 * oO * Oo,1 - 2 * O * O - 2 * Oo * Oo,2 * O * Oo + 2 * oO * o,0,2 * o * Oo + 2 * oO * O,2 * O * Oo - 2 * oO * o,1 - 2 * o * o - 2 * O * O,0,0,0,0,1
+	};
+	glm::mat4 R3 = { 1 - 2 * O * O - 2 * Oo ,2 * o * O + 2 * oO * Oo,2 * o * Oo - 2 * oO * O,0,2 * o * O - 2 * oO * Oo,1 - 2 * o * o - 2 * Oo * Oo,2 * O * Oo + 2 * oO * o,0,2 * o * Oo + 2 * oO * O,2 * O * Oo - 2 * oO * o,1 - 2 * o * o - 2 * O * O,0,0,0,0,1
+	};
+	_viewMatrix = R3 * R * R1 * R2;
+	if (strafing) {
+		glm::vec3 up = glm::vec3(0, 1, 0);
+		glm::vec3 right = glm::normalize(glm::cross(-i, up));
+		up = glm::normalize(glm::cross(right, -i));
+		_strafe += up * float(e) * speed + right * -float(b) * speed;
+	}
+	_position = _position + _strafe;
+	_viewMatrix = glm::inverse(glm::translate(glm::mat4(1.0f), _position) * R);
+	_mouseX = x;
+	_mouseY = y;
+}
+
+//double x, double y, float zoom, bool dragging, bool strafin
+void Camera::update(float zoom, bool dragging, bool strafing) {
+	glfwGetCursorPos(_window, &x, &y);
+	int b = static_cast<int>(x - _mouseX);
+	int e = static_cast<int>(y - _mouseY);
+	int f = static_cast<int>(x + _mouseX);
+	int g = static_cast<int>(y + _mouseX);
+	float speed = 0.005f;
+	glm::vec3 i;
+	if (dragging) {
+		ggg += b * speed;
+		ooo += e * speed;
+		ttt += f * speed;
+		tt += g * speed;
+		ooo = glm::min(ooo, glm::pi<float>() * 0.5f - 0.01f);
+		glm::mat4 t(1);
+		t = glm::translate(t, glm::vec3(-x, -y, 0.0f));
+		t = glm::translate(t, glm::vec3(x, y, 0));
+		glm::vec4 vec0(f, b, 0, 0);
+		vec0 = vec0 * t;
+		ttt = glm::max(tt, glm::pi<float>() * 0.5f - 0.01f);
+		g = vec0.x, f = vec0.y;
+		g = glm::max((float)f, -glm::pi<float>() * 0.5f + 0.01f);
+		glm::vec4 vec1(g, f, 0, 0);
+		ooo = glm::max(ooo, -glm::pi<float>() * 0.5f + 0.01f);
+		float Z = f - g;
+		glm::vec3 z = glm::normalize(glm::vec3(Z));
+		glm::vec3 oi = cross(z, glm::vec3(vec0.x, vec0.y, vec0.z));
+		vec1 = vec1 * t;
+		ttt = glm::min(tt, glm::pi<float>() * 0.5f - 0.01f);
+		f = vec1.x, g = vec1.y;
+	}
+	i.x = zoom * glm::cos(ooo) * -glm::sin(ggg);
+	i.y = zoom * glm::sin(ooo);
+	i.z = zoom * glm::cos(ooo) * glm::cos(ggg);
+	_position = i;
+
+	glm::vec3 d = cross(i, glm::vec3(0, 1, 0));
+	glm::vec3 l = cross(d, i);
+
+
+	glm::vec3 h = glm::normalize(glm::vec3(0.0f, 0.0f, 1.0f));
+	glm::vec3 m = glm::normalize(i);
+	float cosTheta = dot(h, m);
+	glm::vec3 c;
+	c = cross(h, m);
+	float s = sqrt((1 + cosTheta) * 2);
+	float invs = 1 / s;
+	glm::quat rot1 = glm::quat(s * 0.5f,
+		c.x * invs,
+		c.y * invs,
+		c.z * invs);
+	glm::vec3 a = rot1 * glm::vec3(0.0f, 1.0f, 0.0f);
+	h = glm::normalize(a);
+	m = glm::normalize(l);
+	cosTheta = dot(h, m);
+	c = cross(h, m);
+	s = sqrt((1 + cosTheta) * 2);
+	invs = 1 / s;
+	glm::quat rot2 = glm::quat(s * 0.5f,
+		c.x * invs,
+		c.y * invs,
+		c.z * invs);
+	glm::quat q = rot2 * rot1;
+	float oO = q.w;
+	float o = q.x;
+	float O = q.y;
+	float Oo = q.z;
+	glm::mat4 R = { 1 - 2 * O * O - 2 * Oo * Oo,2 * o * O + 2 * oO * Oo,2 * o * Oo - 2 * oO * O,0,2 * o * O - 2 * oO * Oo,1 - 2 * o * o - 2 * Oo * Oo,2 * O * Oo + 2 * oO * o,0,2 * o * Oo + 2 * oO * O,2 * O * Oo - 2 * oO * o,1 - 2 * o * o - 2 * O * O,0,0,0,0,1
+	};
+	glm::mat4 R1 = { 1 - 2 * O * O ,2 * o * O + 2 * oO * Oo,2 * o * Oo - 2 * oO * O,0,2 * o * O - 2 * oO * Oo,1 - 2 * o * o - 2 * Oo * Oo,2 * O * Oo + 2 * oO * o,0,2 * o * Oo + 2 * oO * O,2 * O * Oo - 2 * oO * o,1 - 2 * o * o - 2 * O * O,0,0,0,0,1
+	};
+	glm::mat4 R2 = { 1 - O * O - 2 * Oo * Oo,2 * o * O + 2 * oO * Oo,2 * o * Oo - 2 * oO ,0,2 * o * O - 2 * oO * Oo,1 - 2 * O * O - 2 * Oo * Oo,2 * O * Oo + 2 * oO * o,0,2 * o * Oo + 2 * oO * O,2 * O * Oo - 2 * oO * o,1 - 2 * o * o - 2 * O * O,0,0,0,0,1
 	};
 	glm::mat4 R3 = { 1 - 2 * O * O - 2 * Oo ,2 * o * O + 2 * oO * Oo,2 * o * Oo - 2 * oO * O,0,2 * o * O - 2 * oO * Oo,1 - 2 * o * o - 2 * Oo * Oo,2 * O * Oo + 2 * oO * o,0,2 * o * Oo + 2 * oO * O,2 * O * Oo - 2 * oO * o,1 - 2 * o * o - 2 * O * O,0,0,0,0,1
 	};
--- Camera.h
+++ Camera.h
@@ -8,7 +8,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/euler_angles.hpp>
-
+#include "VulkanLaunchpad.h"
 /*!
  * Arc ball camera, modified by mouse input
  */
@@ -17,22 +17,25 @@ class Camera
 public:
 	/*!
 	 * Camera constructor
+	 * @param	window					The glfw window handle.
 	 * @param	projection_matrix		The projection matrix to be used for this camera
 	 */
-	Camera(glm::mat4 projection_matrix);
+	Camera(GLFWwindow* window, glm::mat4 projection_matrix);
 
 	/*!
 	 * Camera constructor which internally creates a default projection matrix using the 
 	 * aspect ratio calculated from the given window_width and window_height parameters.
+	 * @param	window				The glfw window handle.
 	 * @param	window_width 		The width  of your surface to calculate the aspect ratio for the projection matrix.
 	 * @param	window_height		The height of your surface to calculate the aspect ratio for the projection matrix.
 	 */
-	Camera(int window_width, int window_height);
+	Camera(GLFWwindow* window, int window_width, int window_height);
 
 	/*!
 	 * Camera constructor which internally creates a default projection matrix.
+	 * @param	window					The glfw window handle.
 	 */
-	Camera();
+	Camera(GLFWwindow* window);
 
 	virtual ~Camera();
 
@@ -46,7 +49,7 @@ public:
 	 */
 	glm::mat4 getViewProjectionMatrix() const;
 
-		/*!
+	 /*!
 	 * Updates the camera's position and view matrix according to the input
 	 * @param x			current mouse x position
 	 * @param y			current mouse x position
@@ -56,13 +59,23 @@ public:
 	 */
 	void update(double x, double y, float zoom, bool dragging, bool strafing);
 
+	/*!
+	* Updates the camera's position and view matrix according to the input
+	* @param zoom		zoom multiplier
+	* @param dragging	is the camera dragging
+	* @param strafing	is the camera strafing
+	*/
+	void update(float zoom = 2.0f, bool dragging = true, bool strafing = true);
+
 protected:
 	glm::mat4 _viewMatrix;
 	glm::mat4 _projMatrix;
 	double _mouseX, _mouseY;
+	double x, y;
 	float ooo, ggg;
 	glm::vec3 _position;
 	glm::vec3 _strafe;
 	glm::vec3 ttt;
 	glm::vec3 tt;
+	GLFWwindow* _window;
 };
